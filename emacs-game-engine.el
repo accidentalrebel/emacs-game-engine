5ec961693a06301e7bcc95fd218e2ac8bece3747
ARebel <accidentalrebel@gmail.com>
2020-07-07 09:28:47 +0800

    Created a simple update timer.

--- emacs-game-engine.el
+++ emacs-game-engine.el
@@ -29,15 +29,24 @@
 ;; functions for editing the buffer through col and row coordinates.
 (require 'coordinate)
 
+(defvar ege:buffer-cols 0 "The number of columns for the buffer")
+(defvar ege:buffer-rows 0 "The number of rows for the buffer")
+
 (defun ege:init (buffer-name cols rows)
   "Initialize the engine and the buffer canvas.
 
 BUFFER-NAME is the name of the buffer
 COLS is the number of columns (in characters)
 ROWS is the number of rows (in characters)"
+  (setq ege:buffer-cols cols)
+  (setq ege:buffer-rows rows)
+  
   (pop-to-buffer buffer-name)
+  (ege:clear_window))
+
+(defun ege:clear_window ()
   (erase-buffer)
-  (coordinate-initialize-view-area cols rows "-"))
+  (coordinate-initialize-view-area ege:buffer-cols ege:buffer-rows "-"))
 
 (provide 'emacs-game-engine)
 ;;; emacs-game-engine.el ends here
--- game.el
+++ game.el
@@ -1,5 +1,7 @@
 ;; Load the game engine
 
+(setq x-pos 0)
+
 (when (file-exists-p "emacs-game-engine.el")
   (load-file "./emacs-game-engine.el"))
 
@@ -7,38 +9,52 @@
 
 (ege:init "Example Game" 80 24)
 
-(coordinate-place-string-at-area 1 1 "##    
+(defun draw_header()
+  (coordinate-place-string-at-area 1 1 "##    
 ##    
 ##    
 ##    
 ######")
-(coordinate-place-string-at-area 8 1 "######
+  (coordinate-place-string-at-area 8 1 "######
   ##  
   ##  
   ##  
 ######")
-(coordinate-place-string-at-area 15 1 " #####
+  (coordinate-place-string-at-area 15 1 " #####
 ##    
  #### 
     ##
 ##### ")
-(coordinate-place-string-at-area 22 1 "##### 
+  (coordinate-place-string-at-area 22 1 "##### 
 ##  ##
 ##### 
 ##    
 ##    ")
-(coordinate-place-string-at-area 32 1 "    ##
+  (coordinate-place-string-at-area 32 1 "    ##
     ##
     ##
 ##  ##
  #### ")
-(coordinate-place-string-at-area 39 1 "  ##  
+  (coordinate-place-string-at-area 39 1 "  ##  
  #### 
 ##  ##
 ######
 ##  ##")
-(coordinate-place-string-at-area 46 1 "##    ##
+  (coordinate-place-string-at-area 46 1 "##    ##
 ###  ###
 ## ## ##
 ## ## ##
 ##    ##")
+  )
+
+(defun update()
+  (setq x-pos (+ x-pos 1))
+
+  (ege:clear_window)
+  (draw_header)
+  (coordinate-place-char-at x-pos 7 "x")
+  ;; (message "This is a test"))
+  )
+
+(run-with-timer 1 1 'update)
+
