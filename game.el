5fa0c30f20c5e4e428ac1eb8e4148d6b673c0852
ARebel <accidentalrebel@gmail.com>
2020-07-07 14:50:15 +0800

    Cleaned up and organized the files.

--- game.el
+++ game.el
@@ -1,15 +1,28 @@
-;; Load the game engine
+;;; game.el --- Emacs Game Engine example game    -*- lexical-binding: t -*-
 
-(setq x-pos 0)
+;;; Commentary:
+;; 
 
+;;; Code:
+
+;; Load the game engine library
+;; If the file is in another direcotry then change it here
 (when (file-exists-p "emacs-game-engine.el")
   (load-file "./emacs-game-engine.el"))
 
 (require 'emacs-game-engine)
 
+;; Initialize the engine with the buffer name and number of cols and rows
 (ege:init "Example Game" 80 24)
 
+;; VARIABLES
+;; =========
+(setq x-pos 0)
+
+;; HELPERS
+;; =======
 (defun draw_header()
+  "Draw the header texts."
   (ege:place-string-at-area 1 1 "##    
 ##    
 ##    
@@ -47,6 +60,7 @@
 ##    ##"))
 
 (defun update()
+  "Game update function."
   (ege:clear_buffer) ; Clear the window at the start of the frame
   (message "here1")
 
@@ -58,5 +72,6 @@
     (draw_header)
     (ege:place-char-at x-pos 7 "x")))
 
-;; (run-with-timer 1 1 'update)
 (ege:register_update 'update 24)
+
+;;; game.el ends here
