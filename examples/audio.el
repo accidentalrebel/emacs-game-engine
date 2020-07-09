f2ae2eb6daa1ae5e056b4dc269a3e6853467b40d
ARebel <accidentalrebel@gmail.com>
2020-07-09 15:16:21 +0800

    Created a function that stops a sound.

--- examples/audio.el
+++ examples/audio.el
@@ -1,3 +1,13 @@
+;;; drawing.el --- Emacs Game Engine example game    -*- lexical-binding: t -*-
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
+(require 'emacs-game-engine)
+
+(defvar ege:audio-player "play" "Audio player to use for playing async audio files")
 
 (defun ege:play-sound (path volume)
   "Plays the sound from relative PATH with VOLUME between 0 to 1.
@@ -15,8 +25,26 @@ Use ege:play-sound-async for asynchronous audio playback."
   "Plays the sound asynchronously from relative PATH with VOLUME between 0 to 1.
 Tries to look for specific audio players found in the OS."
   (let ((data-directory (expand-file-name path)))
-    (start-process-shell-command "ege:play-sound-async" nil (concat "play " data-directory)))
-  )
+    (start-process-shell-command (concat "ege:play-sound:" path)
+				 nil
+				 (concat ege:audio-player " " data-directory))))
+
+(defun ege:stop-sound (process)
+  "Stops the given sound PROCESS.
+Process can be a returned object from ege:play-sound-async, it can also be the path to the file."
+  (if (processp process)
+      (kill-process process)
+    (kill-process (concat "ege:play-sound:" process))))
 	       
 (ege:play-sound "./data/audio/coin.wav" 0.5)
+
+;; Plays music asynchronously and then stops it after 3 seconds
 (ege:play-sound-async "./data/audio/cantina.wav" 0.5)
+(sleep-for 3)
+(ege:stop-sound "./data/audio/cantina.wav") ;; Stopping sound by specifying filename
+
+;; Plays music asynchronously and then stops it after 3 seconds
+(setq current-sound (ege:play-sound-async "./data/audio/cantina.wav" 0.5))
+(sleep-for 3)
+(ege:stop-sound current-sound)) ;; Stopping sound by specifying process
+
