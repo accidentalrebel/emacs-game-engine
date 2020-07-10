dd11807814ef2e22c01adf5026a59effbd339720
ARebel <accidentalrebel@gmail.com>
2020-07-10 08:53:26 +0800

    Repositions the cursor on the clicked position.

--- ege-mouse.el
+++ ege-mouse.el
@@ -5,21 +5,26 @@
 
 ;;; Code:
 
+;; TODO: Double clicking highlights the line. Find a way to disable this.
+;; TODO: For buttons to work properly, should make sure that the buffer is on the upper leftmost part of the frame
+
 (defun ege:_setup-mouse-events ()
   (local-set-key [mouse-1] 'ege:_on-mouse-pressed))
 
 (defun ege:mouse-init ()
-  (ege:_setup-mouse-events)
-  )
+  (ege:_setup-mouse-events))
 
 (defun ege:_on-mouse-pressed ()
-  (interactive)
+  ;; TODO; remove interactive
+  (interactive) 
   (message "Clicked")
+  (let ((pos (ege:mouse-position)))
+    (message (concat "Pos " (number-to-string (car pos)) ", " (number-to-string (cdr pos))))
+    (coordinate-position-point-at (car pos) (cdr pos)))
   )
 
 (defun ege:mouse-position ()
-  (mouse-position)
-  )
+  (cdr (mouse-position)))
 
 (provide 'ege-mouse)
 ;;; ege-mouse.el ends here
