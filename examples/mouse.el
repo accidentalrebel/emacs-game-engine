24630d054329a29bbc6d21c0aaf3e4e73958ea9f
ARebel <accidentalrebel@gmail.com>
2020-07-10 20:11:15 +0800

    Can now keep track of clicked count.

--- examples/mouse.el
+++ examples/mouse.el
@@ -10,16 +10,24 @@
 ;; Initialize the engine with the buffer name and number of cols and rows
 (ege:init "Example Game" 80 24)
 
-(setq button-rect '(2 2 8 1))
+(setq button-rect '(2 2 10 1))
+(setq clicked-count 0)
 
-(ege:draw-button " > ON " button-rect " " nil
+(ege:draw-button " > BUTTON " button-rect " " nil
 		 '(:background "orange" :foreground "black"))
 
+(defun update-clicked-count()
+  (ege:draw-text (concat " Clicked count: " (number-to-string clicked-count) " ")
+		 13 2
+		 '(:background "gray" :foreground "black")))
 
 (defun update()
   (when (ege:rect-pressed-p button-rect 1)
-    (message "BUTTON CLICKED")
+    (setq clicked-count (+ clicked-count 1))
+    (update-clicked-count)
     )
-  )
+)
+
+(update-clicked-count)
 
 (ege:register-update 'update 24)
