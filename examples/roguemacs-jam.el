c9a0781a0d5390f3d4c766fff6321adcddfc0589
ARebel <accidentalrebel@gmail.com>
2020-07-12 11:44:43 +0800

    Can now display multiple rows.

--- examples/roguemacs-jam.el
+++ examples/roguemacs-jam.el
@@ -23,16 +23,27 @@ ABCDE           #########           ###############################          ###
 ###      #################### ############  ########### ###########          ###
 ###      ######         ##### ######           ########        ####          ###
 ###############                                ########        ####          ###
-###############         ############           #################################
+###############         ############           ###################### ##########
+##################################################                    ##########
+##################################################  ##### ########### ##########
+#####                                               ##### ########        ######
+##### ###############################################       ######        ######
+#####               ########               ##########       ######        ######
+##################  ########               #######################        ######
+##################                         #######################        ######
+############################               #####################################
+################################################################################
 ################################################################################"
 "Non-procedurally generated map.")
+
+(defvar map-dimensions '(80 21) "Dimensions of the map")
 
 (defun rog:get-map-tiles-from-position (position count)
   "Gets the tiles from the given map POSITION.
 From that position copies the characters up to COUNT."
   (let* ((col (nth 0 position))
 	 (row (nth 1 position))
-	 (index (+ (* row 80) (+ col 1)))
+	 (index (+ (* row (car map-dimensions)) (+ col 1)))
 	 (adjusted-index (+ index (- row 1))))
     (substring rog:map
 	       adjusted-index
@@ -46,10 +57,15 @@ From that position copies the characters up to COUNT."
 	(copy-col (nth 0 copy-rect))
 	(copy-row (nth 1 copy-rect))
 	(copy-width (nth 2 copy-rect))
-	(copy-height (nth 3 copy-rect)))
-    (ege:draw-text (rog:get-map-tiles-from-position (list copy-col copy-row) copy-width)
-		   draw-col
-		   draw-row)
+	(copy-height (nth 3 copy-rect))
+	(y-index 0))
+    (while (< y-index copy-height)
+      (message (number-to-string y-index))
+      (ege:draw-text (rog:get-map-tiles-from-position (list copy-col (+ copy-row y-index)) copy-width)
+		     draw-col
+		     (+ draw-row y-index))
+      (setq y-index (+ y-index 1))
+      )
     )
   )
 
